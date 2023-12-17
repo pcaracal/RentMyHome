61d8be9c35e4d2ed382cfe10f7d9ee31ae9c9fcf
pcaracal <peter.schreivogel@stud.kbw.ch>
2023-12-17 11:38:38 +0100

    feat: get bookings

--- backend/src/routes.rs
+++ backend/src/routes.rs
@@ -1,6 +1,6 @@
 use rocket::serde::json::Json;
 use rocket_http::Status;
-use backend::models::Room;
+use backend::models::{Booking, Room};
 use serde_json::json;
 use diesel::prelude::*;
 use backend::schema;
@@ -18,4 +18,16 @@ pub fn get_rooms() -> (Status, Option<Json<Vec<Room>>>) {
         return (Status::NotFound, None);
     }
     (Status::Ok, Some(Json(rooms)))
+}
+
+#[get("/api/bookings/<room_id>")]
+pub fn get_bookings(room_id: i32) -> (Status, Option<Json<Vec<Booking>>>) {
+    let mut connection = backend::establish_connection();
+
+    let bookings = match schema::booking::table.filter(schema::booking::room_id.eq(room_id)).load::<Booking>(&mut connection) {
+        Ok(bookings) => (Status::Ok, Some(Json(bookings))),
+        Err(_) => return (Status::InternalServerError, None),
+    };
+
+    bookings
 }
